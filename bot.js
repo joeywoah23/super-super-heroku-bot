038677a6e4ebec0310c19f697feb4c1a1f115992
joeywoah23 <58614907+joeywoah23@users.noreply.github.com>
2020-03-15 21:59:21 -0400

    Added music functions

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const ytdl = require('ytdl-core');
 const fs = require("fs");
+const queue = new Map();
 
 console.log("Snoopy Worker has loaded... Commands have been queued...")
 client.on("message", message => {
@@ -599,7 +601,98 @@ if (message.content.startsWith(config.prefix + "hitlist")) {
       message.channel.send({embed: help})
     }});
 
-  
+  client.on('message', async message => {
+  if (message.author.bot) return;
+	  const serverQueue = queue.get(message.guild.id);
+
+if (message.content.startsWith(config.prefix + `play`)) {
+ execute(message, serverQueue);
+ return;
+} else if (message.content.startsWith(config.prefix + `skip`)) {
+ skip(message, serverQueue);
+ return;
+} else if (message.content.startsWith(config.prefix + `stop`)) {
+ stop(message, serverQueue);
+ return;
+} else {
+message.channel.send('You need to enter a valid command!')
+async function execute(message, serverQueue) {
+ const args = message.content.split(' ');
+ const voiceChannel = message.member.voiceChannel;
+ if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+  const permissions =     voiceChannel.permissionsFor(message.client.user);
+ if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+  return message.channel.send('I need the permissions to join and   speak in your voice channel!');
+ }
+}
+const songInfo = await ytdl.getInfo(args[1]);
+const song = {
+ title: songInfo.title,
+ url: songInfo.video_url,
+};
+if (!serverQueue) {
+}else {
+ serverQueue.songs.push(song);
+ console.log(serverQueue.songs);
+ return message.channel.send(`${song.title} has been added to the queue!`);
+}
+// Creating the contract for our queue
+const queueContruct = {
+ textChannel: message.channel,
+ voiceChannel: voiceChannel,
+ connection: null,
+ songs: [],
+ volume: 5,
+ playing: true,
+};
+// Setting the queue using our contract
+queue.set(message.guild.id, queueContruct);
+// Pushing the song to our songs array
+queueContruct.songs.push(song);
+
+try {
+ // Here we try to join the voicechat and save our connection into our object.
+ var connection = await voiceChannel.join();
+ queueContruct.connection = connection;
+ // Calling the play function to start a song
+ play(message.guild, queueContruct.songs[0]);
+} catch (err) {
+ // Printing the error message if the bot fails to join the voicechat
+ console.log(err);
+ queue.delete(message.guild.id);
+ return message.channel.send(err);
+}
+function play(guild, song) {
+ const serverQueue = queue.get(guild.id);
+ if (!song) {
+  serverQueue.voiceChannel.leave();
+  queue.delete(guild.id);
+  return;
+ }
+}
+const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+ .on('end', () => {
+  console.log('Music ended!');
+  // Deletes the finished song from the queue
+  serverQueue.songs.shift();
+  // Calls the play function again with the next song
+  play(guild, serverQueue.songs[0]);
+})
+ .on('error', error => {
+  console.error(error);
+ });
+dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+function skip(message, serverQueue) {
+ if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+ if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+ serverQueue.connection.dispatcher.end();
+}
+function stop(message, serverQueue) {
+ if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+ serverQueue.songs = [];
+ serverQueue.connection.dispatcher.end();
+}
+}
     
 
     client.on('message', message => {
