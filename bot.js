223c1baacadd034cb99b51b5f12396650befe288
joeywoah23 <58614907+joeywoah23@users.noreply.github.com>
2020-03-17 12:38:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const Util = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 const ytdl = require('ytdl-core');
@@ -7,6 +8,8 @@ const getYoutubeID = require('get-youtube-id'); //
 const YouTube = require('simple-youtube-api'); //
 const youtube = new YouTube("AIzaSyD747gqlEB_p4UD-OYKWra5G6E3hP3cuHc"); //
 const fs = require("fs");
+const gif = require("gif-search");
+const client = new Discord.Client({disableEveryone: true});
 const queue = new Map();
 const {
 	prefix,
