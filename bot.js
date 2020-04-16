811d5b80d6295357c28a02d2b6b775a94b831a19
joeywoah23 <58614907+joeywoah23@users.noreply.github.com>
2020-04-16 16:33:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1458,7 +1458,7 @@ const applyText = (canvas, text) => {
 };
 
 client.on('guildMemberAdd', async member => {
-	const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+	const channel = member.guild.channels.find(ch => ch.name === '♡-･ﾟwelcome');
 	if (!channel) return;
 
 	const canvas = Canvas.createCanvas(700, 250);
