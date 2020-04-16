292985db64236e42dadcb50ff3d36809db191002
joeywoah23 <58614907+joeywoah23@users.noreply.github.com>
2020-04-16 15:04:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1094,10 +1094,11 @@ return message.reply("You can\'t punish yourself!");
         }
         
 	if(message.content.startsWith(config.prefix + 'lockdown')) {
-	message.channel.overwritePermissions(message.guild.defaultRole, { SEND_MESSAGES: false });	
+	if (message.channel.overwritePermissions(message.guild.defaultRole, { SEND_MESSAGES: true })).then(message.channel.overwritePermissions(message.guild.defaultRole, { SEND_MESSAGES: false });)	
 	message.channel.send("This channel has been locked down.");
-	} else {
-	message.channel.overwritePermissions(message.guild.defaultRole, { SEND_MESSAGES: true });
+	} else
+	if(message.content.startsWith(config.prefix + 'lockdown')) {
+	if (message.channel.overwritePermissions(message.guild.defaultRole, { SEND_MESSAGES: false })).then(message.channel.overwritePermissions(message.guild.defaultRole, { SEND_MESSAGES: true });)	
 	message.channel.send("This channel has been unlocked.");
 	}
 	
