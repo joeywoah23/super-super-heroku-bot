36b2f15c5e5d59ae2c3e22a3302852d4d5c0c19c
joeywoah23 <58614907+joeywoah23@users.noreply.github.com>
2020-04-17 04:26:58 -0400

    Delete shard.js

--- shard.js
+++
@@ -1,8 +0,0 @@
-/*
-    The following code goes into it's own file, and you run this file
-    instead of your main bot file.
-*/
-
-const Discord = require('discord.js');
-const Manager = new Discord.ShardingManager('./bot.js');
-Manager.spawn(2); // This example will spawn 2 shards (5,000 guilds);
