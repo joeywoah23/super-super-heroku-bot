12b88c98351c89fb4b34da4df44f9010ac6e8d8e
joeywoah23 <58614907+joeywoah23@users.noreply.github.com>
2020-04-17 17:29:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ const getYoutubeID = require('get-youtube-id'); //
 const YouTube = require('simple-youtube-api'); //
 const youtube = new YouTube(process.env.YOUTUBE_API_KEY); //
 const moment = require("moment");
+const getUserProfile = require("./UserProfile.js");
 const fs = require("fs");
 const chalk = require('chalk');
 const Canvas = require('canvas');
